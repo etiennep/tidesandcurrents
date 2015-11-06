e711d4963447a07d52e7c23dd46dd965c76c10a4
Etienne Pelletier <epelletier@twilio.com>
2015-11-06 13:44:10 -0800

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tidesandcurrents:app --log-file=-
---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Sends Bay Area daily tides and currents via SMS',
+    'author': 'Etienne Pelletier',
+    'author_email': 'epelletier@twilio.com',
+    'version': '0.1',
+    'install_requires': [
+          'flask',
+          'twilio'
+      ],
+    'scripts': [],
+    'name': 'tidesandcurrents'
+}
+
+
+setup(**config)
