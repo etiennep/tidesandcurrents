6eba2cf6a2913c639467819c4cdb027da5f6f60d
Etienne Pelletier <epelletier@twilio.com>
2015-11-11 13:45:27 -0800

    Added better timezone support.
    Don't rely on server location to determine what today's date is.

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from __future__ import print_function
 from flask import Flask, request
 import twilio.twiml
-import datetime
+from datetime import datetime
 import tidesandcurrents
+from tidesandcurrents import tzinfo
 
 app = Flask(__name__)
 
 
 def parse_date(date_str):
     clean_date_str = date_str.strip().lower()
-    today = datetime.date.today()
+    today = datetime.now(tzinfo.us_timezones['Pacific']).date()
     if clean_date_str == 'today':
         return today
     elif clean_date_str == 'tomorrow':
         return today + datetime.timedelta(days=1)
     else:
         try:
-            ret = datetime.datetime.strptime(clean_date_str, '%m/%d/%Y').date()
+            ret = datetime.strptime(clean_date_str, '%m/%d/%Y').date()
             return ret if ret >= today else None
         except ValueError:
             return None
@@ -38,8 +39,9 @@ def tides_and_currents():
     try:
         tides = tidesandcurrents.query_tides(query_date)
         currents = tidesandcurrents.query_currents(query_date)
-        message = 'Tide and current predictions for {}\nTides:\n{}\nCurrents:\n{}'.format(query_date.strftime('%m/%d/%Y'),
-                                                                                          tides, currents)
+        message = 'Tide and current predictions for ' \
+                  '{}\nTides:\n{}\nCurrents:\n{}'.format(query_date.strftime('%m/%d/%Y'),
+                                                         tides, currents)
         resp.message(message).media(tidesandcurrents.media_link)
     except:
         resp.message("Error trying to retrieve tide and current predictions. Please try again later.")
